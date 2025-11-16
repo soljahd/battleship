5e8cebff6f4378907358f2590a22621fd5a41688
soljah <moskuitoz@gmail.com>
2025-11-17 00:54:56 +0300

    feat: winners sort implemented

--- src/ws_server/utils.ts
+++ src/ws_server/utils.ts
@@ -32,7 +32,8 @@ export function updateRoomsBroadcast(connections: Map<WsWebSocket, string | null
 export function updateWinnersBroadcast(connections: Map<WsWebSocket, string | null>, users: Map<string, PlayerRecord>) {
   const winnersList = Array.from(users.values())
     .map((user) => ({ name: user.name, wins: user.wins }))
-    .filter(({ wins }) => wins > 0);
+    .filter(({ wins }) => wins > 0)
+    .sort((a, b) => b.wins - a.wins);
   broadcastToAll(connections, 'update_winners', winnersList);
 }
 
